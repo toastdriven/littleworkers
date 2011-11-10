d84d2304d739c89e38cdc4cf28d60192b143189b
Daniel Lindsley <daniel@toastdriven.com>
2011-11-10 17:57:15 -0600

    Bumped to v0.3.1!

--- littleworkers.py
+++ littleworkers.py
@@ -5,7 +5,7 @@ import time
 
 
 __author__ = 'Daniel Lindsley'
-__version__ = (0, 3, 0)
+__version__ = (0, 3, 1)
 __license__ = 'BSD'
 
 
@@ -20,9 +20,9 @@ class NotEnoughWorkers(LittleWorkersException):
 class Pool(object):
     """
     The main pool object. Manages a set of specified workers.
-    
+
     Usage::
-    
+
         commands = [
             'ls -al',
             'cd /tmp && mkdir foo',
@@ -32,46 +32,46 @@ class Pool(object):
         ]
         lil = Pool(workers=2)
         lil.run(commands)
-    
+
     Optionally accepts a ``workers`` kwarg. Default is 1.
-    
+
     Optionally accepts a ``debug`` kwarg. Default is False.
-    
+
     Optionally accepts a ``wait_time`` kwarg. Default is 0.1.
     """
     def __init__(self, workers=1, debug=False, wait_time=0.1):
         if workers < 1:
             raise NotEnoughWorkers("You need to use at least one worker.")
-        
+
         self.workers = workers
         self.pool = {}
         self.commands = []
         self.callback = None
         self.debug = debug
         self.wait_time = wait_time
-    
+
     def prepare_commands(self, commands):
         """
         A hook to override how the commands are added.
-        
+
         By default, simply copies the provided command ``list`` to the
         internal ``commands`` list.
         """
         # Make a copy of the commands to run.
         self.commands = commands[:]
-    
+
     def command_count(self):
         """
         Returns the number of commands to be run.
-        
+
         Useful as a hook if you use a different structure for the commands.
         """
         return len(self.commands)
-    
+
     def next_command(self):
         """
         Fetches the next command for processing.
-        
+
         Will return ``None`` if there are no commands remaining (unless
         ``Pool.debug = True``).
         """
@@ -80,9 +80,9 @@ class Pool(object):
         except IndexError:
             if self.debug:
                 raise
-        
+
         return None
-    
+
     def process_kwargs(self, command):
         """
         A hook to alter the kwargs given to ``subprocess.Process``.
@@ -95,7 +95,7 @@ class Pool(object):
         return {
             'shell': True,
         }
-    
+
     def create_process(self, command):
         """
         Given a provided command (string or list), creates a new process
@@ -104,7 +104,7 @@ class Pool(object):
         logging.debug("Starting process to handle command '%s'." % command)
         kwargs = self.process_kwargs(command)
         return subprocess.Popen(command, **kwargs)
-    
+
     def set_callback(self, callback=None):
         """
         Sets up a callback to be run whenever a process finishes.
@@ -113,18 +113,18 @@ class Pool(object):
         existing callback.
         """
         self.callback = callback
-    
+
     def add_to_pool(self, proc):
         """
         Adds a process to the pool.
         """
         logging.debug("Adding %s to the pool." % proc.pid)
         self.pool[proc.pid] = proc
-    
+
     def remove_from_pool(self, pid):
         """
         Removes a process to the pool.
-        
+
         Fails silently if the process id is no longer present (unless
         ``Pool.debug = True``).
         """
@@ -134,11 +134,11 @@ class Pool(object):
         except KeyError:
             if self.debug:
                 raise
-    
+
     def inspect_pool(self):
         """
         A hook for inspecting the pool's current status.
-        
+
         By default, simply makes a log message and returns the length of
         the pool.
         """
@@ -146,19 +146,19 @@ class Pool(object):
         pool_size = len(self.pool)
         logging.debug("Current pool size: %s" % pool_size)
         return pool_size
-    
+
     def busy_wait(self):
         """
         A hook to control how often the busy-wait loop runs.
-        
+
         By default, sleeps for 0.1 seconds.
         """
         time.sleep(self.wait_time)
-    
+
     def run(self, commands=None, callback=None):
         """
         The method to actually execute all the commands with the pool.
-        
+
         Optionally accepts a ``commands`` kwarg, as a shortcut not to have to
         call ``Pool.prepare_commands``.
         """
@@ -167,31 +167,31 @@ class Pool(object):
 
         if callback is not None:
             self.set_callback(callback)
-        
+
         keep_running = True
-        
+
         while keep_running:
             self.inspect_pool()
-            
+
             if len(self.pool) <= min(self.command_count(), self.workers):
                 command = self.next_command()
-                
+
                 if not command:
                     self.busy_wait()
                     continue
-                
+
                 proc = self.create_process(command)
                 self.add_to_pool(proc)
-            
+
             # Go in reverse order so offsets never get screwed up.
             for pid in self.pool.keys():
                 logging.debug("Checking status on %s" % self.pool[pid].pid)
-                
+
                 if self.pool[pid].poll() >= 0:
                     if self.callback:
                         self.callback(self.pool[pid])
 
                     self.remove_from_pool(pid)
-            
+
             keep_running = self.command_count() or len(self.pool) > 0
             self.busy_wait()
--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from distutils.core import setup
 
 setup(
     name='littleworkers',
-    version='0.3.0',
+    version='0.3.1',
     description='Little process-based workers to do your bidding.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
+    long_description=open('README.rst', 'r').read(),
+    url='https://github.com/toastdriven/littleworkers',
     py_modules = ['littleworkers'],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
