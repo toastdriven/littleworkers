a6f3bdb278bb0c6d9bdb570c0979a02852fa2454
Daniel Lindsley <daniel@toastdriven.com>
2013-12-03 02:09:00 -0800

    Added initial wheel support.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='littleworkers',
